6e16acfa39a3e8da7b9014fa629931b6ee27644a
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2023-01-11 21:32:22 -0400

    Add latest photos to public page

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,2 +1,8 @@
 module PropertiesHelper
+    def property_thumbnail(property)
+        img = property.photo.present? ? property.photo.thumb.url : '/images/placeholder.png'
+        image_tag img, class: "property-thumb"
+    end
 end
+
+
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,7 @@
 class Property < ApplicationRecord
     include ImageUploader::Attachment(:image)
-  
+    belongs_to :account
+
+    scope :latest, -> { order(created_at: :desc) }
 end
+
