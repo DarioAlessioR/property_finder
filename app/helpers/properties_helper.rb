10c7612aa5608897601bf7672485af23c382670f
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2023-01-14 18:14:09 -0400

    Update latest photos placeholder

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,6 +1,6 @@
 module PropertiesHelper
     def property_thumbnail(property)
-        img = property.photo.present? ? property.photo.thumb.url : '/images/placeholder.png'
+        img = property.photo.present? ? property.photo.thumb.url : 'placeholder.png'
         image_tag img, class: "property-thumb"
     end
 end
