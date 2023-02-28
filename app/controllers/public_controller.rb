ee6037e0b87d583316ab4fd6719f6654a277274c
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2023-02-28 13:35:11 -0400

    add filter in home

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,9 @@
 class PublicController < ApplicationController
   def main
     @properties = Property.latest
+
+    if params[:min_price].present? && params[:max_price].present?
+      @properties = @properties.where(price: params[:min_price]..params[:max_price])
+    end
   end
 end
