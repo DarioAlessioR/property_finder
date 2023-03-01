9d81892fece0e639ae93ffde4e3924138fe973a8
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2023-02-28 20:25:28 -0400

    update public

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -5,5 +5,26 @@ class PublicController < ApplicationController
     if params[:min_price].present? && params[:max_price].present?
       @properties = @properties.where(price: params[:min_price]..params[:max_price])
     end
+
+    if params[:min_rooms].present? && params[:max_rooms].present?
+      @properties = @properties.where(rooms: params[:min_rooms]..params[:max_rooms])
+    end
+
+    if params[:min_bathrooms].present? && params[:max_bathrooms].present?
+      @properties = @properties.where(bathrooms: params[:min_bathrooms]..params[:max_bathrooms])
+    end
+
+    if params[:min_area].present? && params[:max_area].present?
+      @properties = @properties.where(area: params[:min_area]..params[:max_area])
+    end
+
+    if params[:kind].present?
+      @properties = @properties.where(kind: params[:kind])
+    end
+
+
+
+
+
   end
 end
