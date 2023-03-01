f7f0a9aa525a75878bc769d0fac7bcd41455c682
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2023-02-28 21:20:01 -0400

    Update public_controller.rb

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -18,6 +18,14 @@ class PublicController < ApplicationController
       @properties = @properties.where(rooms: params[:min_rooms]..params[:max_rooms])
     end
 
+    if params[:min_rooms].present? && params[:max_rooms].blank?
+      @properties = @properties.where(rooms: params[:min_rooms]..params[9999999999999999])
+    end
+
+    if params[:min_rooms].blank? && params[:max_rooms].present?
+      @properties = @properties.where(rooms: params[0]..params[:max_rooms])
+    end
+
     if params[:min_bathrooms].present? && params[:max_bathrooms].present?
       @properties = @properties.where(bathrooms: params[:min_bathrooms]..params[:max_bathrooms])
     end
